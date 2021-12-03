04491dd135154fa63b3b66d1fb5c1ffbb55dceb7
Ross Sponholtz <rsponholtz@hotmail.com>
2021-12-02 17:37:38 -0800

    initial version of small 02

---
+++ 2021/advent21-02-small.lisp
@@ -0,0 +1,51 @@
+(defpackage :advent21-02-small
+  (:use :cl))
+
+(ql:quickload :cl-ppcre)
+(ql:quickload :lisp-unit)
+(ql:quickload :uiop)
+(ql:quickload :alexandria)
+
+(in-package :advent21-02-small)
+
+(defparameter *hpos* 0)
+(defparameter *dpos* 0)
+(defparameter *aim* 0)
+
+(defun proc-line (str)
+  (let* ((tmpstr (cl-ppcre:split "\ " str) )
+         (tmpcmd (car tmpstr))
+         (tmpparm (parse-integer (car (cdr tmpstr)))))
+    (cond 
+      ((string= tmpcmd '"forward") (setq *hpos* (+ *hpos* tmpparm)))
+      ((string= tmpcmd '"down")    (setq *dpos* (+ *dpos* tmpparm)))
+      ((string= tmpcmd '"up")      (setq *dpos* (- *dpos* tmpparm)))
+      )))
+    
+
+(defun proc-input ()
+  (progn
+    (setq *dpos* 0)
+    (setq *hpos* 0)
+    (setq *aim* 0)    
+    (mapcar #'proc-line (uiop:read-file-lines "inputs/input21-02.txt"))
+    (* *hpos* *dpos*)))
+
+;;part 1
+(write (proc-input))
+
+(defun proc-line (str)
+  (let* ((tmpstr (cl-ppcre:split "\ " str) )
+         (tmpcmd (car tmpstr))
+         (tmpparm (parse-integer (car (cdr tmpstr)))))
+    (cond 
+      ((string= tmpcmd '"forward") (progn (setq *hpos* (+ *hpos* tmpparm))
+                                          (setq *dpos* (+ *dpos* (* *aim* tmpparm)))))
+      ((string= tmpcmd '"down")    (setq *aim* (+ *aim* tmpparm)))
+      ((string= tmpcmd '"up")      (setq *aim* (- *aim* tmpparm)))
+      )))
+
+;;part 2
+(write (proc-input))
+
+
